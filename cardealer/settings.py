41afd1f56a67ae55692216543623d560638f5c19
Akshar <aksharp955@gmail.com>
2023-04-27 17:32:21 +0530

    2nd commit

--- cardealer/settings.py
+++ cardealer/settings.py
@@ -166,7 +166,8 @@ SITE_ID = 1
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587 
+EMAIL_PORT = 587
+
 
 
 # Whitenoise settings
